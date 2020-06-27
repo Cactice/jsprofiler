6921bff8b618058504c1be8570c64b43adaa191e
Cactice <14835424+Cactice@users.noreply.github.com>
2020-06-27 19:42:50 +0900

    Updates

---
+++ _next/static/msM7GlDET3yuvkVH0S-PR/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a,b,c){return {"/":[a,b,c],"/index":[a,b,c],"/navbar/navbar":[a,b,c]}}("static\u002Fcss\u002F8f286b92.56604180.chunk.css","static\u002Fchunks\u002F8f286b92.399f109fdec6a50191d5.js","static\u002Fchunks\u002F7840bcae5878bae45c3eb17f20eb857b81f91bb1.982f2ae9f03d5fff3ed0.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
